9327d5dce41a09b874b0a8db4166625cf99500f4
Carlos Gonser <carlos.gonser@doclerholding.com>
2020-11-06 00:30:38 +0100

    add nelmio cors

--- config/bundles.php
+++ config/bundles.php
@@ -12,4 +12,5 @@ return [
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
     Liip\TestFixturesBundle\LiipTestFixturesBundle::class => ['dev' => true, 'test' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
 ];
