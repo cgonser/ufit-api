98f5f7586888a986ad51be881570dbaa3e5c49d3
Carlos Gonser <carlos@ufit.io>
2021-08-23 19:00:27 +0200

    vendor photo - return vendor dto upon photo upload

--- src/Vendor/Controller/VendorPhotoController.php
+++ src/Vendor/Controller/VendorPhotoController.php
@@ -4,7 +4,9 @@ namespace App\Vendor\Controller;
 
 use App\Core\Exception\ApiJsonException;
 use App\Core\Response\ApiJsonResponse;
+use App\Vendor\Dto\VendorDto;
 use App\Vendor\Entity\Vendor;
+use App\Vendor\ResponseMapper\VendorResponseMapper;
 use App\Vendor\Service\VendorPhotoManager;
 use Nelmio\ApiDocBundle\Annotation\Model;
 use OpenApi\Annotations as OA;
@@ -16,11 +18,14 @@ use Symfony\Component\Routing\Annotation\Route;
 class VendorPhotoController extends AbstractController
 {
     private VendorPhotoManager $vendorPhotoManager;
+    private VendorResponseMapper $vendorResponseMapper;
 
     public function __construct(
-        VendorPhotoManager $vendorPhotoManager
+        VendorPhotoManager $vendorPhotoManager,
+        VendorResponseMapper $vendorResponseMapper
     ) {
         $this->vendorPhotoManager = $vendorPhotoManager;
+        $this->vendorResponseMapper = $vendorResponseMapper;
     }
 
     /**
@@ -28,7 +33,7 @@ class VendorPhotoController extends AbstractController
      *
      * @OA\Tag(name="Vendor")
      * @OA\RequestBody(required=true, @OA\MediaType(mediaType="application/octet-stream", @OA\Schema(type="string", format="binary")))
-     * @OA\Response(response=204, description="Uploads a new photo")
+     * @OA\Response(response=200, description="Success", @OA\JsonContent(ref=@Model(type=VendorDto::class)))
      * @OA\Response(response=400, description="Invalid input")
      */
     public function create(
@@ -46,7 +51,8 @@ class VendorPhotoController extends AbstractController
         $this->vendorPhotoManager->uploadPhoto($vendor, $request->getContent());
 
         return new ApiJsonResponse(
-            Response::HTTP_NO_CONTENT
+            Response::HTTP_OK,
+            $this->vendorResponseMapper->map($vendor)
         );
     }
 }
